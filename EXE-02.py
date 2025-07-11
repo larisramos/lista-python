258761859e7a2dfb1ec90684a36d405d36dd38bd
larisramos <larissaramos438@gmail.com>
2025-07-11 10:14:50 -0300

    Revisão Python

---
+++ EXE-02.py
@@ -0,0 +1,18 @@
+'''
+Autor(a): Larissa Ramos
+Data: 11/07/2025
+
+2 - Quatro Operações: Elabore um programa que receba dois números e mostre a soma, subtração, multiplicação e divisão entre eles.'''
+
+'''ENTRADA DE VALORES'''
+print("-------QUATRO OPERAÇÕES:-------\n")
+valor1 = float(input("1° Valor = "))
+valor2 = float(input("2° Valor ="))
+print("\n")
+'''PROCESSAMENTO'''
+soma = valor1+valor2
+subtracao = valor1-valor2
+multiplicacao = valor1*valor2
+divisao = valor1/valor2
+'''SAÍDA'''
+print("\nSOMA = ", soma,"\nSUBTRAÇÃO = ", subtracao, "\nMULTIPLICAÇÃO = ", multiplicacao, "\nDIVISÃO = ", divisao)
