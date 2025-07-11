b4acf56f7650089d6439274d96152341975a0f7d
larisramos <larissaramos438@gmail.com>
2025-07-11 16:59:38 -0300

    Revisão Python

---
+++ EXE-03.py
@@ -0,0 +1,13 @@
+'''
+Autora: Larissa Ramos
+Data: 11/07/2025
+
+3 - Cálculo de Média: Peça ao usuário para inserir três notas e calcule a média aritmética.
+'''
+print("------CÁLCULO DE MÉDIA------\n")
+nota1, nota2, nota3 = float(input("Insira suas 3 notas - APERTE A TECLA ENTER:\n"))
+media = (nota1+nota2+nota3)/3
+print("\nMÉDIA = ", media)
+
+
+'''NÃO TERMINADO'''
